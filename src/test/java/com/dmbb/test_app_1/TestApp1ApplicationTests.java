365d13a91b9b8997fa9fa2befba17cc141166df4
Dmitrii <happycorner616@gmail.com>
2021-01-25 18:24:10 +0200

    init commit

---
+++ src/test/java/com/dmbb/test_app_1/TestApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmbb.test_app_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
