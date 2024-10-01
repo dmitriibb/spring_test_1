556d8be4420aab47baa8833c4e5b264f34ee7441
dmitriibb <54529797+dmitriibb@users.noreply.github.com>
2024-10-01 14:30:52 +0500

    Feature 2 (#3)

--- src/main/java/com/dmbb/test_app_1/config/Conf.java
+++ src/main/java/com/dmbb/test_app_1/config/Conf.java
@@ -7,6 +7,9 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class Conf {
 
+    public static String HELLO = "hello";
+    public static String HELLO_2 = "hello2";
+    
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
